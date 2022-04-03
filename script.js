adbb1ed94b7a4bd849357c183423c9920e7e547d
MatheusGCM <nexteu10@outlook.com>
2022-04-02 23:50:15 -0300

    Update files

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 const display = document.querySelector(".display");
-const resposta = document.querySelector(".btnResp");
 
-function insert(valor){
-    display.innerHTML += valor;
-}
-
-function clean(){
+function insert(valor) {
+  if (display.innerHTML === "0") {
     display.innerHTML = "";
+    display.innerHTML += valor;
+  } else {
+    display.innerHTML += valor;
+  }
 }
 
-function backspace(){
-    if(display.textContent){
-        let aux = document.getElementById("display").innerHTML;
-        display.innerHTML = aux.substring(0, aux.length -1);
-    }
+function clean() {
+  display.innerHTML = "0";
 }
 
-function result(){
-    if(display.textContent != 'Erro'){
-        document.getElementById("display").innerHTML = eval(display.innerHTML).toFixed(2);
-        let aux = document.getElementById("display").innerHTML;
-        if(aux.length > 5){
-            document.getElementById("display").innerHTML = eval(display.innerHTML).toFixed(2);
-        }else{
-            document.getElementById("display").innerHTML = eval(display.innerHTML);
-        }
+function backspace() {
+  if (display.textContent) {
+    let aux = document.getElementById("display").innerHTML;
+    display.innerHTML = aux.substring(0, aux.length - 1);
+  }
+}
+
+function result() {
+  if (display.textContent != "Erro") {
+    document.getElementById("display").innerHTML = eval(
+      display.innerHTML
+    ).toFixed(2);
+    let aux = document.getElementById("display").innerHTML;
+    if (aux.length > 5) {
+      document.getElementById("display").innerHTML = eval(
+        display.innerHTML
+      ).toFixed(2);
+    } else {
+      document.getElementById("display").innerHTML = eval(display.innerHTML);
     }
+  }
 }
